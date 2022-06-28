68a1040e4fb7be5988c169216ed36d55cb1385ea
ahmet92akinsel <ahmet.akinsel@gmail.com>
2022-06-28 12:59:24 +0300

    progress

--- src/components/Sx.jsx
+++ src/components/Sx.jsx
@@ -4,6 +4,26 @@ import { Button, TextField, Grid, Typography } from "@mui/material";
 
 //Colors: #00203F, #ADEFD1,#FFFFFF
 
+//Typography styles
+// color: "#00203F",
+// fontFamily: "Montserrat",
+// fontWeight: "500",
+// fontSize: "42px",
+// letterSpacing: "2px",
+// lineHeight: "44px",
+
+//Button styles
+// color: "#FFFFFF",
+// width: "175px",
+// height: "40px",
+// fontSize: "20px",
+// fontWeight: "800",
+// borderRadius: "5px",
+// fontFamily: "Montserrat",
+// letterSpacing: "4px",
+// backgroundColor: "#00203F",
+// marginTop: "20px",
+
 const Styling = () => {
   return (
     <>
@@ -22,7 +42,7 @@ const Styling = () => {
         }}
       >
         <Grid item>
-          <Typography>The Sx Prop</Typography>
+          <Typography>Styling Options</Typography>
         </Grid>
         <Grid item>
           <TextField variant="outlined"></TextField>
